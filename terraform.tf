500a9354c40961fe81683edc84b75f94fb1b7f56
rskchowdhurycloud <76244205+rskchowdhurycloud@users.noreply.github.com>
2021-10-23 13:02:46 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+ provider "aws" {
+ region = "us-east-1"
+}
+
+resource "aws_instance" "app-server1" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  key_name = "vodafone-devops"
+  provisioner "local-exec" {
+  command = "echo ${aws_instance.app-server1.public_ip} > ./inventory"
+  }
+  tags = {
+    Name = "Suresh-server-devops-v1"
+    dept = "Engineering"
+    env = "Dev"
+  }
+}

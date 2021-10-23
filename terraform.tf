43af7728a6d766cc0831aaab1781abfcdb75ac42
rskchowdhurycloud <76244205+rskchowdhurycloud@users.noreply.github.com>
2021-10-23 13:18:54 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,7 @@
 resource "aws_instance" "app-server1" {
   ami           = "ami-02e136e904f3da870"
   instance_type = "t2.micro"
-  key_name = "vodafone-devops"
+  key_name = "Suresh-key"
   provisioner "local-exec" {
   command = "echo ${aws_instance.app-server1.public_ip} > ./inventory"
   }
